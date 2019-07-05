f471965c9c8c60a034e7fd1820c4b709a265c5a9
ThomFDV <t.ferreira@monsieurbiz.com>
2019-07-05 11:52:46 +0200

    Update route testing by importing modules

--- src/app/app-routing.spec.ts
+++ src/app/app-routing.spec.ts
@@ -3,7 +3,7 @@ import {TestBed, fakeAsync, tick} from '@angular/core/testing';
 import {RouterTestingModule} from '@angular/router/testing';
 import {Router} from '@angular/router';
 import {routes} from './app-routing.module';
-import {HttpClientModule} from '@angular/common/http';
+import {HttpClient, HttpClientModule} from '@angular/common/http';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './core/components/home/home.component';
 import {LoginComponent} from './core/components/login/login.component';
@@ -11,6 +11,14 @@ import {FooterComponent} from './core/components/footer/footer.component';
 import {HeaderComponent} from './core/components/header/header.component';
 import {NotFoundComponent} from './core/components/not-found/not-found.component';
 import {SearchSomethingComponent} from './core/components/search-something/search-something.component';
+import {ButtonModule} from 'primeng/button';
+import {ToastModule} from 'primeng/toast';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {TranslateLoader, TranslateModule, TranslateService, TranslateStore} from '@ngx-translate/core';
+import {NoNamePipe} from './core/services/pipes/no-name.pipe';
+import {LocalStorageService} from './core/services/localstorage/localstorage.service';
+import {HttpLoaderFactory} from './app.module';
+import {MessageService} from 'primeng/api';
 
 describe('Router: App', () => {
     let location: Location;
@@ -19,7 +27,21 @@ describe('Router: App', () => {
 
     beforeEach(() => {
         TestBed.configureTestingModule({
-            imports: [RouterTestingModule.withRoutes(routes), HttpClientModule],
+            imports: [
+                RouterTestingModule.withRoutes(routes),
+                HttpClientModule,
+                ButtonModule,
+                ToastModule,
+                ReactiveFormsModule,
+                FormsModule,
+                TranslateModule.forRoot({
+                    loader: {
+                        provide: TranslateLoader,
+                        useFactory: HttpLoaderFactory,
+                        deps: [HttpClient]
+                    }
+                }),
+            ],
             declarations: [
                 AppComponent,
                 HomeComponent,
@@ -27,8 +49,16 @@ describe('Router: App', () => {
                 FooterComponent,
                 HeaderComponent,
                 NotFoundComponent,
-                SearchSomethingComponent
+                SearchSomethingComponent,
+                NoNamePipe
             ],
+            providers: [
+                LocalStorageService,
+                TranslateService,
+                TranslateStore,
+                MessageService,
+                NoNamePipe
+            ]
         });
 
         router = TestBed.get(Router);
